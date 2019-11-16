6a9342235fe535870dd1f20330a55636be7f5eab
Ruben Catalan Gutierrez <byrubenss@gmail.com>
2019-11-15 20:36:20 -0600

    CRUD

---
+++ AlumnosCRUD/AlumnosCRUD/Alumnos 5B/Alumnos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace AlumnosCRUD.Alumnos_5B
+{
+    class Alumnos
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Area { get; set; }
+        public string Edad { get; set; }
+        public string Sexo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Nombre} - {Area} - {Edad}-{Sexo}";
+        }
+    }
+}
